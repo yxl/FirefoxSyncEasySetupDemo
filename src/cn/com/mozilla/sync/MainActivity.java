bfe679ad78fac2ce9a7237afa0ca35302b5310a3
Yuan Xulei <xyuan@mozilla.com>
2011-12-15 21:46:44 +0800

    add license instruction and clean up the code.

--- src/cn/com/mozilla/sync/MainActivity.java
+++ src/cn/com/mozilla/sync/MainActivity.java
@@ -1,3 +1,39 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is FirefoxSyncEasySetupDemo.
+ *
+ * The Initial Developer of the Original Code is
+ * the Mozilla Foundation.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *  Yuan Xulei <xyuan@mozilla.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 package cn.com.mozilla.sync;
 
 import java.util.concurrent.atomic.AtomicReference;
@@ -8,7 +44,6 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import cn.com.mozilla.sync.task.EasySetupStep;
@@ -19,10 +54,6 @@ import cn.com.mozilla.sync.task.IEasySetupListener;
  * Easy Setup activity.
  */
 public class MainActivity extends Activity implements IEasySetupListener {
-
-  // Constant used to retrieve start activity result of
-  // SyncPreferencesActivity
-  private static final int                            WEAVE_PREFERENCES_ACTIVITY = 0;
 
   private String                                      mPIN                       = "";
 
@@ -52,13 +83,6 @@ public class MainActivity extends Activity implements IEasySetupListener {
     mPIN3EditText.setText("");
 
     mProgressDialog = new ProgressDialog(this);
-  }
-
-  private void afterWeavePreferencesActivity(int resultCode) {
-    if (resultCode == RESULT_OK) {
-      setResult(RESULT_OK);
-      finish();
-    }
   }
 
   /*
@@ -196,22 +220,5 @@ public class MainActivity extends Activity implements IEasySetupListener {
     default:
       mProgressDialog.setMessage(step.toString());
     }
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see android.app.Activity#onActivityResult(int, int,
-   * android.content.Intent)
-   */
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    switch (requestCode) {
-    case WEAVE_PREFERENCES_ACTIVITY: {
-      afterWeavePreferencesActivity(resultCode);
-      return;
-    }
-    }
-    super.onActivityResult(requestCode, resultCode, data);
   }
 }
